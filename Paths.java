08406b2de01ffaacd51b0ac36f6223ab6dec903e
xiaoyizhou <1846662839@qq.com>
2024-05-04 22:37:02 +0800

    以前采用的数据结构过于臃肿，将文件处理后决定重构项目

---
+++ Paths.java
@@ -0,0 +1,30 @@
+package 大作业;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Paths {
+    private int id;
+    private List<Site> list;
+
+    public List<Site> getList() {
+        return list;
+    }
+
+    public void setList(List<Site> list) {
+        this.list = list;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public Paths(int id) {
+        this.id = id;
+        this.list = new ArrayList<Site>();
+    }
+
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
